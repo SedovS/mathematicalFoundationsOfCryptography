0618d0e76e96cd31c2f1eb9bc460900c6ffa6f27
SedovS <Neusedelka@yandex.ru>
2019-09-26 17:05:46 +0300

    add inverse element VC

---
+++ mathematicalFoundationsOfCryptography/InverseElementViewController.swift
@@ -0,0 +1,43 @@
+//
+//  InverseElementViewController.swift
+//  mathematicalFoundationsOfCryptography
+//
+//  Created by Apple on 26/09/2019.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+
+class InverseElementViewController: UIViewController {
+
+    @IBOutlet weak var numberTextField: UITextField!
+    @IBOutlet weak var modulTextField: UITextField!
+    
+    @IBAction func searchButton(_ sender: UIButton) {
+        search()
+    }
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        resultLabel.text = ""
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    private func search() -> Void {
+        guard let number = Int(numberTextField.text!) else {return}
+        guard let modul = Int(modulTextField.text!) else {return}
+        guard number != 0 else {return}
+        guard modul != 0 else {return}
+        view.endEditing(true) //убираем клавиатуру
+        
+        guard let result = Algorithms.findInverseElement(number, modul) else {resultLabel.text = "(\(number),\(modul) != 1 /n Нельзя найти обратный элемент "
+            return
+        }
+        resultLabel.text = "Обратный элемент равен \n \(result)"
+    }
+
+    
+
+}
