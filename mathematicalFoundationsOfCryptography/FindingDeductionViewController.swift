29c517a9eff12a87273d38bd0d3678c2dad6784e
SedovS <Neusedelka@yandex.ru>
2019-10-06 15:54:03 +0300

    add finding deductor VC and logic

--- mathematicalFoundationsOfCryptography/FindingDeductionViewController.swift
+++ mathematicalFoundationsOfCryptography/FindingDeductionViewController.swift
@@ -10,21 +10,35 @@ import UIKit
 
 class FindingDeductionViewController: UIViewController {
 
+    @IBAction func searchButton(_ sender: UIButton) {
+        search()
+    }
+    @IBOutlet weak var modulTextField: UITextField!
+    @IBOutlet weak var numberTextField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var exponentTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        resultLabel.text = ""
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func search() -> Void {
+        
+        resultLabel.text = ""
+        guard let number = Int(numberTextField.text!) else {return}
+        guard let modul = Int(modulTextField.text!) else {return}
+        guard let exponent = Int(exponentTextField.text!) else {return}
+        if number == 0 || modul == 0 || exponent == 0 {return}
+        view.endEditing(true) //убираем клавиатуру
+        
+        if Algorithms.functionEulers(modul) == modul-1 {
+            resultLabel.text = "\(Algorithms.findingDeductionSimple(a: number, k: exponent, modul: modul)) (mod \(modul))"
+        } else {
+            resultLabel.text = Algorithms.findingDeductionСomposite(a: number, k: exponent, modul: modul)
+        }
+        
     }
-    */
 
 }
