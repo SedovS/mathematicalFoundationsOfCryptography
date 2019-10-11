b0bcdb01fcd1ee73fef141e0e12076ee928fdefb
SedovS <Neusedelka@yandex.ru>
2019-10-11 21:04:38 +0300

    Доделан алгоритм и перенесен в класс с алгоритмами

--- mathematicalFoundationsOfCryptography/Algorithms VC/DecisionComparisonsViewController.swift
+++ mathematicalFoundationsOfCryptography/Algorithms VC/DecisionComparisonsViewController.swift
@@ -19,6 +19,7 @@ class DecisionComparisonsViewController: UIViewController {
         result()
     }
     let scrollView = UIScrollView()
+    var label = UILabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,44 +44,32 @@ class DecisionComparisonsViewController: UIViewController {
         
         if let inverseElement = Algorithms.findInverseElement(a, modul) {
             //m простое
-            resultLabel.text = "X= \(b*inverseElement%modul) + \(modul)k, k∈Z"
+            resultLabel.text = Algorithms.comparisonsSimpleM(inverseElement: inverseElement, b: b, modul: modul)
         } else {
             //m составное
-            compositeM(a: a, b: b, modul: modul)
+             compositeM(text: Algorithms.comparisonsCompoundM(a: a, b: b, modul: modul))
         }
     }
     
-    private func compositeM(a : Int, b : Int, modul : Int) -> Void {
-        let euclid = Euclid()
-        let d = euclid.greatestCommonDivisor(a, modul)
-        if b%d != 0 {
-            resultLabel.text = "d|b условие теоремы не выполнятеся, \n т.к. \(d) не делит нацело \(b) \n \(b)%\(d)=\(b%d) "
-            return
-        }
-        
+   
+    private func compositeM(text: String) -> Void {
+        label.removeFromSuperview()
         let contentView = UIView()
-        let width = view.frame.width
-        let height = 21
-        var y = 0
-        for i in 0..<d {
-            let label = UILabel()
-            label.frame = CGRect(x: 0, y: y, width: Int(width), height: height)
-            y += height + 2
-            label.text = "X\(i + 1)= \(b/d + ((i)*(modul/d))) + \(modul)k, k∈Z\n"
-            label.font = UIFont.systemFont(ofSize: 20)
-            label.textAlignment = .center
-            contentView.addSubview(label)
-        }
-        
-        let widthScrolView = view.frame.width - 2*20
+       
+        label = UILabel()
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.text = text
+        label.font = UIFont.systemFont(ofSize: 20)
+        label.sizeToFit()
+
+        contentView.addSubview(label)
         let heightScrolView = view.frame.height - 310
-        scrollView.frame = CGRect(x: 20, y: 310, width: widthScrolView, height: heightScrolView)
-        contentView.frame = CGRect(x: 0, y: 0, width: Int(widthScrolView), height: y)
+        scrollView.frame = CGRect(x: 0, y: 310, width: view.frame.width, height: heightScrolView)
+        contentView.frame = CGRect(x: ((view.frame.width / 2) - (label.frame.width / 2) + 5), y: 0, width: view.frame.width , height: label.frame.height)
         scrollView.contentSize.height = contentView.frame.height // Or whatever you want it to be.
         scrollView.addSubview(contentView)
         view.addSubview(scrollView)
     }
-
-   
-
+    
 }
