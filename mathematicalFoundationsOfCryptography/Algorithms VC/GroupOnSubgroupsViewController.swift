060148bb9ea3c0fc65926891bc7652c081d38542
SedovS <Neusedelka@yandex.ru>
2019-11-07 22:42:59 +0300

    add elements

--- mathematicalFoundationsOfCryptography/Algorithms VC/GroupOnSubgroupsViewController.swift
+++ mathematicalFoundationsOfCryptography/Algorithms VC/GroupOnSubgroupsViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class GroupOnSubgroupsViewController: UIViewController {
 
+    @IBOutlet weak var numberTextField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func additionButton(_ sender: UIButton) {
+    }
+    @IBAction func multiplacationButton(_ sender: UIButton) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
