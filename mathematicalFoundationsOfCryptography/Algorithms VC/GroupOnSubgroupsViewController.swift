7b8be1026603ccd4d552caf648a7d2fcb78991f0
SedovS <Neusedelka@yandex.ru>
2019-11-08 00:46:43 +0300

    add realization VC

--- mathematicalFoundationsOfCryptography/Algorithms VC/GroupOnSubgroupsViewController.swift
+++ mathematicalFoundationsOfCryptography/Algorithms VC/GroupOnSubgroupsViewController.swift
@@ -14,25 +14,46 @@ class GroupOnSubgroupsViewController: UIViewController {
     @IBOutlet weak var resultLabel: UILabel!
     
     @IBAction func additionButton(_ sender: UIButton) {
+        search(operation: "+")
     }
     @IBAction func multiplacationButton(_ sender: UIButton) {
+        search(operation: "*")
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        resultLabel.text = ""
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func search(operation: String) -> Void {
+        resultLabel.text = ""
+        guard let number = Int(numberTextField.text!), number != 0 else {return}
+        view.endEditing(true)
+        
+        switch operation {
+        case "+":
+            textResult(group: Group.decompositionGroupOnSubgroupsOnAddition(element: number))
+            break
+        case "*":
+            textResult(group: Group.decompositionGroupOnSubgroupsOnMultiplication(element: number))
+            
+            break
+        default:
+            return
+        }
     }
-    */
-
+    
+    private func textResult(group : Dictionary<Int, [[Int]]>) -> Void {
+        
+        let dict = group.sorted(by: { $0.key < $1.key})
+        var result : String = ""
+        
+        for arrays in dict {
+            result += "Подгруппа "
+            result += "\(arrays.value) \n"
+        }
+        resultLabel.text = result
+    }
 }
