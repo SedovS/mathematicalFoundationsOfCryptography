08ebcc0b779b62217eca7454e6194b7d99a9d4d1
SedovS <Neusedelka@yandex.ru>
2019-11-16 13:37:34 +0300

    add model and view for 3 task

--- mathematicalFoundationsOfCryptography/Model.swift
+++ mathematicalFoundationsOfCryptography/Model.swift
@@ -21,18 +21,19 @@ class Model {
 
     static public func getModel() -> [Model] {
         return [Model(name: "Алгоритм Евклида", about: "by sergey_sedov 1-я лекция", seguaIdentifier: "mainPageToEuclid"),
-        Model(name: "Каноническое разложение числа \n Функция Эйлера", about: "by sergey_sedov 2-я лекция", seguaIdentifier: "mainPageToEulerAbdCDN"),
-        Model(name: "Нахождение обатного элемента", about: "by sergey_sedov 3-я лекция", seguaIdentifier: "mainPageToInverseElement"),
-        Model(name: "Решение сравненией (для простого и составного m)", about: "by sergey_sedov 3-я лекция", seguaIdentifier: "mainPageToDicisionComparisons"),
-        Model(name: "Китайская теорема", about:"by sergey_sedov 4-я лекция" , seguaIdentifier: "mainPageToChineseTheorem"),
-        Model(name: "Нахождение вычета", about: "by sergey_sedov 5-я лекция", seguaIdentifier: "mainPageToFindingDeduction"),
-        Model(name: "Нахождение первообразного корня", about: "by sergey_sedov 5-я лекция", seguaIdentifier: "mainPageToAntiderivativeRoot"),
-        Model(name: "Решенеи степенного (показательного) уравнения", about: "by sergey_sedov 6-я лекция", seguaIdentifier: "mainPageToExponentialComparison"),
-        Model(name: "Нахождение символа Лежандра и символа Якоби", about: "by sergey_sedov 6-я лекция", seguaIdentifier: "mainPageToSymbolLandJ"),
-        Model(name: "Нахождение порядка всех элементов в группе Zm+ и Zm*", about: "by sergey_sedov 8-я лекция", seguaIdentifier: "mainPageToOrderGroup"),
-        Model(name: "Разложение группы на подгруппы и формирование для каждой подгруппы смежных классов (для Zm+ и Zm*)", about: "by sergey_sedov 8-я лекция", seguaIdentifier: "mainPageToGroupOnSubgroups")
+                Model(name: "Поиск простых чисел до числа n", about: "by sergey_sedov 2-я лекция", seguaIdentifier: "mainPageToPrimeNumber"),
+                Model(name: "Каноническое разложение числа \n Функция Эйлера", about: "by sergey_sedov 2-я лекция", seguaIdentifier: "mainPageToEulerAbdCDN"),
+                Model(name: "Нахождение обатного элемента", about: "by sergey_sedov 3-я лекция", seguaIdentifier: "mainPageToInverseElement"),
+                Model(name: "Решение сравненией (для простого и составного m)", about: "by sergey_sedov 3-я лекция", seguaIdentifier: "mainPageToDicisionComparisons"),
+                Model(name: "Китайская теорема", about:"by sergey_sedov 4-я лекция" , seguaIdentifier: "mainPageToChineseTheorem"),
+                Model(name: "Нахождение вычета", about: "by sergey_sedov 5-я лекция", seguaIdentifier: "mainPageToFindingDeduction"),
+                Model(name: "Нахождение первообразного корня", about: "by sergey_sedov 5-я лекция", seguaIdentifier: "mainPageToAntiderivativeRoot"),
+                Model(name: "Решенеи степенного (показательного) уравнения", about: "by sergey_sedov 6-я лекция", seguaIdentifier: "mainPageToExponentialComparison"),
+                Model(name: "Нахождение символа Лежандра и символа Якоби", about: "by sergey_sedov 6-я лекция", seguaIdentifier: "mainPageToSymbolLandJ"),
+                Model(name: "Нахождение порядка всех элементов в группе Zm+ и Zm*", about: "by sergey_sedov 8-я лекция", seguaIdentifier: "mainPageToOrderGroup"),
+                Model(name: "Разложение группы на подгруппы и формирование для каждой подгруппы смежных классов (для Zm+ и Zm*)", about: "by sergey_sedov 8-я лекция", seguaIdentifier: "mainPageToGroupOnSubgroups")
         ]
     }
-
+    
 }
 
